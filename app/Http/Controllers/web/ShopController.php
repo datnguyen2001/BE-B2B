8df4e32783bc4b3f4f02ca06e89331d2855420d8
hiepduc24089 <146322141+hiepduc24089@users.noreply.github.com>
2024-11-03 09:55:05 +0700

    update Shopcontroller

--- app/Http/Controllers/web/ShopController.php
+++ app/Http/Controllers/web/ShopController.php
@@ -25,13 +25,24 @@ class ShopController extends Controller
                 ->leftJoin('follow_shops as fs', 's.id', '=', 'fs.shop_id')
                 ->where('s.id', $id)
                 ->select(
-                    's.*',
+                    's.id',
+                    's.user_id',
+                    's.address_detail',
+                    's.display',
                     DB::raw("CONCAT(s.address_detail, ', ', w.name, ', ', d.name, ', ', p.name) as full_address"),
                     DB::raw("CONCAT( d.name, ', ', p.name) as sub_address"),
                     DB::raw("COUNT(fs.id) as total_followers_shop"),
                     DB::raw('(SELECT COUNT(*) FROM conversations WHERE (user1_id = s.user_id OR user2_id = s.user_id)) as total_contacts')
                 )
-                ->groupBy('s.id')
+                ->groupBy(
+                    's.id',
+                    's.user_id',
+                    's.address_detail',
+                    's.display',
+                    'w.name',
+                    'd.name',
+                    'p.name'
+                )
                 ->first();
             if ($data->display == 0){
                 return response()->json(['message'=>'Cửa hàng của bạn đã bị khóa','status'=>true]);
